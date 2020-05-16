505afa2bd80105ce6b7d87908eb2d7f87fae9a4e
Gabden <Gabdne5545@gmail.com>
2020-05-16 12:04:49 +0300

    add store

---
+++ pages/login.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-card width="400" class="mx-auto mt-5">
+    <v-card-title class="grey lighten-4">
+      <h1 class="display-1 mx-auto">Личный кабинет</h1>
+    </v-card-title>
+    <p v-if="error" class="red--text text-center mt-3">{{ errorMsg }}</p>
+    <v-card-text>
+      <v-form v-model="formValidity" @submit.prevent="login">
+        <v-text-field
+          v-model="username"
+          label="Email"
+          prepend-icon="mdi-account-circle"
+          :rules="usernameRules"
+          class="mt-3"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Пароль"
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="passwordRules"
+          class="mb-3"
+          required
+          @click:append="showPassword = !showPassword"
+        ></v-text-field>
+        <v-card-actions>
+          <v-btn color="success" type="submit" :disabled="!formValidity"
+            >Войти</v-btn
+          >
+          <v-spacer></v-spacer>
+          <router-link router to="/">Забыли пароль?</router-link>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions class="caption grey lighten-4">
+      <span>Еще нет аккаунта?</span>
+      <v-spacer></v-spacer>
+      <router-link router to="/">Зарегистрироваться</router-link>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formValidity: false,
+      showPassword: false,
+      username: '',
+      password: '',
+      error: false,
+      errorMsg: 'Неправильный адрес электронной почты или пароль',
+      usernameRules: [
+        (value) => !!value || 'Необходимо ввести адрес электронной почты',
+        (value) => value.indexOf('@') !== 0 || 'Email должен содержать имя',
+        (value) => value.includes('@') || 'Email должен включать @ символ',
+        (value) =>
+          value.indexOf('.') - value.indexOf('@') > 1 ||
+          'Email должен содержать имя домена',
+        (value) =>
+          value.includes('.') || 'Email должен содержать точку после домена',
+        (value) =>
+          value.indexOf('.') <= value.length - 3 ||
+          'Email должен содержать доменную зону'
+      ],
+      passwordRules: [(value) => !!value || 'Необходимо ввести пароль']
+    }
+  },
+  methods: {
+    login() {
+      this.$store
+        .dispatch('login', {
+          username: this.username,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({ name: 'secured' })
+        })
+        // eslint-disable-next-line handle-callback-err
+        .catch((err) => {
+          this.error = true
+        })
+    }
+  }
+}
+</script>
+
+<style></style>
