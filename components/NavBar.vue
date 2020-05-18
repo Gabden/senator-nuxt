82792b1ae3544465edfbb3185c7580ab4f5c64bd
Gabden <Gabdne5545@gmail.com>
2020-05-18 18:56:27 +0300

    add slide animation

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,31 +1,30 @@
 <template>
   <v-app-bar app dark color="info">
     <v-toolbar-title>Senator</v-toolbar-title>
-    <v-spacer></v-spacer>
     <v-btn text rounded router to="/">Home</v-btn>
     <v-btn text rounded router to="/secured">Secured</v-btn>
     <v-btn text rounded router to="/admin">Only for admin</v-btn>
+    <v-btn v-if="isAdmin" rounded to="/registration">Администрирование</v-btn>
+
+    <v-spacer></v-spacer>
     <v-btn v-if="$auth.loggedIn">{{ $auth.user.username }}</v-btn>
     <v-btn v-if="$auth.loggedIn" text rounded router to="/" @click="logout"
       >Logout</v-btn
     >
-
-    <v-btn v-else text rounded to="/login">Login</v-btn>
-    <v-btn rounded to="/registration">Регистрация</v-btn>
+    <v-btn v-if="!$auth.loggedIn" text rounded to="/login">Login</v-btn>
+    <v-btn v-if="!$auth.loggedIn" rounded to="/registration">Регистрация</v-btn>
   </v-app-bar>
 </template>
 
 <script>
-import { authComputed } from '../services/helpers'
 import requestsService from '../services/requestsService'
 export default {
   computed: {
-    ...authComputed
+    isAdmin() {
+      return this.$auth.user ? this.$auth.user.roles.includes('ADMIN') : false
+    }
   },
   methods: {
-    show() {
-      console.log(authComputed)
-    },
     logout() {
       this.$auth
         .logout()
