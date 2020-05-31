b6b3b536b877a41ff6e252b6b64fee1d842faf8c
Gabden <Gabdne5545@gmail.com>
2020-05-31 19:16:37 +0300

    index feareus

---
+++ components/FeaturesCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="my-5">
+    <v-card-title class="headline">
+      <v-icon>{{ card.icon }}</v-icon
+      >{{ card.content }}</v-card-title
+    >
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,9 @@
     </v-carousel>
 
     <v-row>
-      <v-col cols="6">
-        <h1 class="my-5 text-center">
-          SEN<span class="display-2">A</span>TOR — бутик лучших вин мира и
+      <v-col :cols="$vuetify.breakpoint.smAndDown ? 12 : 6" class="my-5">
+        <h1 class="text-center">
+          СЕН<span class="display-2">A</span>ТОР — бутик лучших вин мира и
           подарков
           <v-icon color="#d50000" class="display-3 mb-1">mdi-glass-wine</v-icon>
         </h1>
@@ -30,18 +30,49 @@
           подарочные наборы. Все материалы, размещенные на сайте, носят
           информационный характер и не являются рекламой.
         </p>
+        <features-card
+          v-for="card in featuresCards"
+          :key="card.icon"
+          :card="card"
+        />
       </v-col>
-      <v-col cols="6">
-        <v-img src="faceSenator.jpg" alt="Senator shop enter"></v-img>
+      <v-col cols="6" class="hidden-sm-and-down">
+        <img
+          src="../assets/faceSenator.jpg"
+          alt="Senator shop enter"
+          style="width: 100%"
+        />
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import FeaturesCard from '@/components/FeaturesCard.vue'
 export default {
+  components: {
+    'features-card': FeaturesCard
+  },
   data() {
     return {
+      featuresCards: [
+        {
+          icon: 'mdi-package',
+          content: 'Более 1000 товаров премиум класса'
+        },
+        {
+          icon: 'mdi-package',
+          content: 'Более 1000 товаров премиум класса'
+        },
+        {
+          icon: 'mdi-package',
+          content: 'Более 1000 товаров премиум класса'
+        },
+        {
+          icon: 'mdi-package',
+          content: 'Более 1000 товаров премиум класса'
+        }
+      ],
       colors: [
         'indigo',
         'warning',
