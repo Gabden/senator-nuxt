00c9769969784c5e45a37c3cbb0a3155693f5443
Gabden <Gabdne5545@gmail.com>
2020-07-05 17:57:19 +0300

    header acc page

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,17 +1,45 @@
 <template>
   <v-row>
     <div>
-      <img src="../assets/default-profile.png" alt="Profile" />
+      <img
+        src="../../assets/default-profile.png"
+        alt="Profile"
+        width="100"
+        height="100"
+      />
     </div>
-    <v-col>
-      <h3 style="font-size: 1.75rem">Габдулин Денис Валерьевич</h3>
-      <p class="subtitle">gabden5545@gmail.com</p>
+    <v-col :class="smallSize ? 'mt-5' : 'mt-3'">
+      <h3 :class="smallSize ? 'name-sm' : 'name-md'">
+        Габдулин Денис Валерьевич
+      </h3>
+      <p :class="smallSize ? 'mail-sm' : 'mail-md'">
+        gabden5545@gmail.com
+      </p>
     </v-col>
   </v-row>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    smallSize() {
+      return this.$vuetify.breakpoint.smAndDown
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped>
+.name-sm {
+  font-size: 20px;
+}
+.name-md {
+  font-size: 1.75rem;
+}
+.mail-sm {
+  font-size: 18px;
+}
+.mail-sm {
+  font-size: 14px;
+}
+</style>
