1996b2940b101766035faecf48d61b3f384decc2
Gabden <Gabdne5545@gmail.com>
2020-08-09 20:26:31 +0300

    auth header

---
+++ middleware/auth-header.js
@@ -0,0 +1,10 @@
+export default function(context) {
+  const tokenCookie = context.req.headers.cookie
+    .split(';')
+    .find((cook) => cook.includes('auth._token.local'))
+
+  if (tokenCookie) {
+    const token = tokenCookie.split('=')[1]
+    context.$axios.defaults.headers.common.Authorization = token
+  }
+}
