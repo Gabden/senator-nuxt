7f160d9adbe478b7b9f2fd42ad887bf7f92ee258
Gabden <Gabdne5545@gmail.com>
2020-08-09 19:23:28 +0300

    startpage banners

---
+++ pages/admin/banners/create.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1 class="text-center">Создать баннер стартовой страницы</h1>
+    <v-form @submit.prevent="createBanner">
+      <v-file-input multiple label="File input"></v-file-input>
+      <v-text-field
+        v-model="url"
+        label="Cсылка для перехода"
+        prepend-icon="mdi-link-variant"
+        class="my-3"
+        required
+      ></v-text-field>
+      <v-btn color="success" type="submit">Создать</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: ''
+    }
+  },
+  methods: {
+    createBanner() {
+      console.log('Create banner click')
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/admin/banners/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 class="text-center">Страница администрирования стартовых баннеров</h1>
+    <h3 class="text-center">
+      Здесь вы можете создать, удалить или отредактировать баннеры
+    </h3>
+    <div class="d-flex justify-center my-5">
+      <v-btn link to="/admin/banners/create" outlined color="green darken-1"
+        >Создать</v-btn
+      >
+    </div>
+    <v-divider></v-divider>
+    <BannerListItem v-for="n in 5" :key="n" class="banner-border mb-5" />
+  </div>
+</template>
+
+<script>
+import BannerListItem from '@/components/admin/BannerListItem'
+export default {
+  middleware: 'auth-admin',
+  components: {
+    BannerListItem
+  }
+}
+</script>
+
+<style scoped>
+.banner-border {
+  border-left: 5px solid green;
+}
+</style>
