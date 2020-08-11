c92ae1766ffd1041d30e79d4d8111c42c400095c
Gabden <Gabdne5545@gmail.com>
2020-08-11 20:44:14 +0300

    cart jumbotron

---
+++ pages/cart/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto text-center" color="green lighten-5">
+      <v-card-text class="font-weight-bold">
+        <p class="green--text text--darken-4 display-1">
+          Внимание!
+        </p>
+        <p>
+          При заказе от
+          <span class="red--text text--accent-4"> 3 ЕДИНИЦ ТОВАРА</span> из
+          категории алкоголь действует скидка
+          <span class="red--text text--accent-4">15%</span>
+        </p>
+        <p>
+          При заказе от
+          <span class="red--text text--accent-4"> 6 ЕДИНИЦ ТОВАРА</span> из
+          категории алкоголь действует скидка
+          <span class="red--text text--accent-4">20%</span>
+        </p>
+        <v-divider class="mx-auto mb-5" style="max-width: 80%"></v-divider>
+        <p class="mb-1">
+          Скидка активируется автоматически при выполнении условий описанных
+          выше
+        </p>
+        <p class="mb-1">
+          Дополнительная скидка не применяется на товары по акции
+        </p>
+        <p class="ma-0">
+          Зарезервированный товар вы сможете получить в магазине Сенатор
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
