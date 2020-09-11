8b78b815b423ede217fe0a0f82533d7eb26950d6
Gabden <Gabdne5545@gmail.com>
2020-09-11 15:55:02 +0300

    responsive improvements search page

--- pages/search/_text.vue
+++ pages/search/_text.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <v-row>
-      <v-col cols="3">
+      <v-col cols="12" md="3">
         <FilterProduct
           :types="types"
           :manufacturers="manufacturers"
           :countries="countries"
+          @filter="filterProducts"
         />
       </v-col>
-      <v-col cols="9">
-        <div>
+      <v-col cols="12" md="9">
+        <p
+          v-if="!products.content || products.content.length === 0"
+          class="display-2 text-center my-5 grey--text"
+        >
+          Ничего не удалось найти
+        </p>
+        <div v-if="products.content && products.content.length > 0">
           <p class="grey--text">Всего найдено: {{ products.totalElements }}</p>
         </div>
 
@@ -17,7 +24,10 @@
           <v-col
             v-for="(product, index) in products.content"
             :key="index"
-            cols="3"
+            cols="10"
+            sm="6"
+            md="4"
+            xl="3"
             class="mx-auto"
           >
             <product-card :product="product"
@@ -25,6 +35,7 @@
         </v-row>
 
         <v-pagination
+          v-if="products.content && products.content.length > 0"
           v-model="page"
           :length="products.totalPages"
           :total-visible="7"
@@ -106,6 +117,21 @@ export default {
       this.$store.commit('SWITCH_LOADER', true)
       this.$axios
         .get(`/api/home/notifications?page=${this.page - 1}`)
+        .then((response) => {
+          this.$store.commit('SWITCH_LOADER', false)
+          this.products = response.data
+        })
+        .catch((e) => {
+          this.$store.commit('SWITCH_LOADER', false)
+          this.$store.$toasted
+            .error('Сервер временно недоступен, повторите попытку позже!')
+            .goAway(2000)
+        })
+    },
+    filterProducts(payload) {
+      this.$store.commit('SWITCH_LOADER', true)
+      this.$axios
+        .post(`/api/product/filter`, payload)
         .then((response) => {
           this.$store.commit('SWITCH_LOADER', false)
           this.products = response.data
