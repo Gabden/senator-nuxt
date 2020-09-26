b19a6f4d1b2319cb2d628c28d2b3c742c3a570fe
Gabden <Gabdne5545@gmail.com>
2020-09-26 17:47:20 +0300

    add logout to expired token

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,6 +1,15 @@
 export default function(context) {
   context.$axios.onError((error) => {
-    if (error.response.status === 401 || error.response.status === 403) {
+    let isExpired = false
+    if (error.response.data && error.response.data.message) {
+      isExpired = error.response.data.message.includes('expired')
+    }
+    console.log(isExpired)
+    if (
+      error.response.status === 401 ||
+      error.response.status === 403 ||
+      isExpired
+    ) {
       context.$auth.logout()
     }
   })
