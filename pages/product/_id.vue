2903a6c3060fdf5c571383ed36ebf3f9aeb55ed5
Gabden <Gabdne5545@gmail.com>
2020-09-30 11:11:45 +0300

    find by temperature

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -273,7 +273,14 @@
             <p class="pl-4 mb-0 grey--text" style="font-size: 0.85rem">
               Температура
             </p>
-            <v-btn color="red" text link to="/" class="text-truncate"
+            <v-btn
+              color="red"
+              text
+              nuxt
+              :to="
+                `/search/temperature?value=${product.productDetails.productAlcoholTemperature}`
+              "
+              class="text-truncate"
               ><span
                 class="d-inline-block text-truncate"
                 style="max-width: 160px;"
