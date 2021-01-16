2531105be6fa34f01c22b633029c65060329b480
Gabden <Gabdne5545@gmail.com>
2021-01-16 18:30:18 +0300

    add pm2 restart memory limit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'senator',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
+      max_memory_restart: '500M',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
